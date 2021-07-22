675696067aecd55e21c860a733b9d723d0838ebc
Angela0203 <84201715+Angela0203@users.noreply.github.com>
2021-07-22 15:16:05 -0400

    tarea_pendiente1

---
+++ FundamentosRails/curso68/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :direccions
+  resources :mensajes
+  resources :rols
+  resources :usuarios
+  resources :articulos
+  resources :clientes
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
