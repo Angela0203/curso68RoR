084f37c338f9f036f04a6313c9479a6928a9e391
Angela0203 <84201715+Angela0203@users.noreply.github.com>
2021-07-26 12:34:49 -0400

    cambios5

---
+++ Rails/FundamentosRails/comida/db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'a.saldivia0203@gmail.com ', password: 'hola123456', password_confirmation: 'hola123456') if Rails.env.development?
